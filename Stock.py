933d4d72d45878b7ef2d3724210273fe4521d785
divypandey <divypandey999@gmail.com>
2019-09-19 10:46:02 +0530

    Divy

--- Stock.py
+++ Stock.py
@@ -1,7 +1,7 @@
 from Strategy import Strategy
 class Stock:
     stock_price = [10,20,30,40,50,60,10,20,10]
-
+    #ho to raha tha br commit
     def __init__(self, instrument_token):
         self.instrument_token = instrument_token
 
